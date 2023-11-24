c2a6ad27fd192144068f8bb6175c6fa84f5b8da1
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-11-24 11:17:05 +0200

    Fixing Toast Component

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,14 +1,14 @@
 @if(Session::has('success'))
-<div class="max-w-sm mx-auto mt-4">
-<div class="bg-green-500 text-white px-4 py-2 mb-3 rounded-md shadow-md">
+<div class="max-w-lg mx-auto mt-4 mb-4">
+<div class="bg-green-500 text-white px-4 py-4 mb-3 text-center rounded-md shadow-md">
     <p class="text-sm">{{Session::get('success')}}</p>
 </div>
 </div>
 @endif
 
 @if(Session::has('error'))
-<div class="max-w-sm mx-auto mt-4">
-<div class="bg-red-500 text-white px-4 py-2 mb-3 rounded-md shadow-md">
+<div class="max-w-lg mx-auto mt-4 mb-4">
+<div class="bg-red-500 text-white px-4 py-4 mb-3 text-center rounded-md shadow-md">
     <p class="text-sm">{{Session::get('error')}}</p>
 </div>
 </div>
