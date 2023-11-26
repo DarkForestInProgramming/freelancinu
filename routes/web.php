5b9146a506b59c3203de267429df74f380fbc58a
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-11-26 15:56:40 +0200

    Fixing Routes

--- routes/web.php
+++ routes/web.php
@@ -25,50 +25,55 @@ Route::fallback(function () {
     return redirect('/');
 });
 
+//@desc Public routes
+
 Route::get('/', function() {
     return view('pages.home');
 })->name('home');
 
+Route::get('/get-subcategories/{category}', [CategoryController::class, 'getSubcategories']);
+Route::get('/get-subsubcategories/{subcategory}', [CategoryController::class, 'getSubsubcategories']);
+Route::get('/post/{post}', [PostController::class, 'singlePostPage']);
+Route::get('/profile/{user:username}', [ProfileController::class, 'profilePage']);
+
 //@desc Email related rooutes
 
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $req) {
-    $req->fulfill();
-    return redirect('/')->with('success', 'El. paštas sėkmingai patvirtintas!');
-})->middleware(['signed'])->name('verification.verify');
-
+Route::middleware(['signed'])->group(function () {
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $req) {
+        $req->fulfill();
+        return redirect('/')->with('success', 'El. paštas sėkmingai patvirtintas!');
+    })->name('verification.verify');
+});
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/verify', [VerificationController::class, 'verifyPage'])->name('verification.notice');
     Route::get('/resend/verification/email', [VerificationController::class, 'resendEmail']);
 });
 
-// @desc Category related routes
+ //@desc Auth/Guest related routes
 
-Route::get('/get-subcategories/{category}', [CategoryController::class, 'getSubcategories']);
-Route::get('/get-subsubcategories/{subcategory}', [CategoryController::class, 'getSubsubcategories']);
+Route::middleware('guest')->group(function () { 
+Route::get('/register', [AuthController::class, 'registerPage']);
+Route::get('/login', [AuthController::class, 'loginPage'])->name('login');
+Route::post('/register', [AuthController::class, 'registerForm']);
+Route::post('/login', [AuthController::class, 'loginForm']);
+});
 
-//@desc Auth related routes
-
-Route::get('/register', [AuthController::class, 'registerPage'])->middleware('guest');
-Route::get('/login', [AuthController::class, 'loginPage'])->name('login')->middleware('guest');
-
-Route::post('/register', [AuthController::class, 'registerForm'])->middleware('guest');
-Route::post('/login', [AuthController::class, 'loginForm'])->middleware('guest');
 Route::post('/logout', [AuthController::class, 'logoutForm'])->middleware('auth');
 
 // Post related routes
 
-Route::get('/create-post', [PostController::class, 'createPostPage'])->middleware('auth', 'verified');
-Route::get('/post/{post}', [PostController::class, 'singlePostPage']);
-
-Route::post('/create-post', [PostController::class, 'storeNewPost'])->middleware('auth', 'verified');
-Route::delete('/post/{post}', [PostController::class, 'deletePost'])->middleware('can:delete,post');
-Route::get('/post/{post}/edit', [PostController::class, 'editPostPage'])->middleware('can:update,post');
-Route::put('/post/{post}', [PostController::class, 'updatePost'])->middleware('can:update,post');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/create-post', [PostController::class, 'createPostPage']);
+    Route::post('/create-post', [PostController::class, 'storeNewPost']);
+    Route::delete('/post/{post}', [PostController::class, 'deletePost'])->middleware('can:delete,post');
+    Route::get('/post/{post}/edit', [PostController::class, 'editPostPage'])->middleware('can:update,post');
+    Route::put('/post/{post}', [PostController::class, 'updatePost'])->middleware('can:update,post');
+});
 
 // Profile related routes
 
-Route::get('/profile/{user:username}', [ProfileController::class, 'profilePage']);
-Route::get('/change-avatar', [ProfileController::class, 'avatarPage'])->middleware('auth', 'verified');
-Route::post('/change-avatar', [ProfileController::class, 'changeAvatar'])->middleware('auth', 'verified');
-
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/change-avatar', [ProfileController::class, 'avatarPage']);
+    Route::post('/change-avatar', [ProfileController::class, 'changeAvatar']);
+});
