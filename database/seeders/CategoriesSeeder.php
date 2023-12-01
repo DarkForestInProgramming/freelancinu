1339b612ba356e438eb9452dda06c54b1ae2feba
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-12-01 20:59:24 +0200

    Styling And Fixing UI

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -15,10 +15,10 @@ class CategoriesSeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            ['name' => 'Klientų Įgijimas', 'description' => 'Efektyvios strategijos ir metodai naujų klientų pritraukimui į jūsų verslą.'],
-            ['name' => 'Programavimas', 'description' => 'Mokymai, naujovės ir patarimai programavimo srityje, skirti tiek pradedantiesiems, tiek patyrusiems programuotojams.'],
-            ['name' => 'Investavimas', 'description' => 'Įsigykite finansinę laisvę ir ekonominį saugumą - atraskite patarimus, gudrybes ir naujausius įvykius finansų pasaulyje.'],
-            ['name' => 'Sportas', 'description' => 'Įkvėpkite aktyvų gyvenimo būdą - atraskite naudingus patarimus, treniruočių programas ir naujausius sporto įvykius.'],
+            ['name' => 'Klientų Įgijimas', 'description' => 'Efektyvios strategijos ir metodai naujų klientų pritraukimui į jūsų verslą.', 'image' => 'https://res.cloudinary.com/dp0m5mp1s/image/upload/v1701455134/Freelancinu/klientu_igijimas.png'],
+            ['name' => 'Programavimas', 'description' => 'Mokymai, naujovės ir patarimai programavimo srityje, skirti tiek pradedantiesiems, tiek patyrusiems programuotojams.', 'image' => 'https://res.cloudinary.com/dp0m5mp1s/image/upload/v1701455342/Freelancinu/programavimas_logo.png'],
+            ['name' => 'Investavimas', 'description' => 'Įsigykite finansinę laisvę ir ekonominį saugumą - atraskite patarimus, gudrybes ir naujausius įvykius finansų pasaulyje.', 'image' => 'https://res.cloudinary.com/dp0m5mp1s/image/upload/v1701455475/Freelancinu/investavimas_logo.png'],
+            ['name' => 'Sportas', 'description' => 'Įkvėpkite aktyvų gyvenimo būdą - atraskite naudingus patarimus, treniruočių programas ir naujausius sporto įvykius.', 'image' => 'https://res.cloudinary.com/dp0m5mp1s/image/upload/v1701455758/Freelancinu/sportas_logo.png'],
         ];
 
         foreach ($categories as $category) {
